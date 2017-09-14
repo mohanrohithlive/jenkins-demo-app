9b9414bb356e8c1b6ddad39c37bba3ea078b6f98
Rohith Mohan <mohanrohith@gmail.com>
2017-09-14 13:08:01 +0530

    Fixed lint issues

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-
-  render 
-  (){
-
-    return ( 
-        <div className="App">
+  render () {
+    return (
+      <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Jenkins Demo App</h2>
