d9798642f4bab24fdf2b9705c3e4d8ae464926de
Rohith Mohan <mohanrohith@gmail.com>
2017-09-14 08:12:15 +0530

    eslint

--- src/App.js
+++ src/App.js
@@ -3,26 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() 
-  
-  
-  {
 
-    
-    return (
-      <div className="App">
+  render 
+  (){
+
+    return ( 
+        <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to Jenkins Demo App</h2>
-            </div>
-            <p className="App-intro">
-          I have changed something here
-          I have changed something here
-          I have changed something here
-          I have changed something he
-          v
-        </p>
-            </div>
+        </div>
+        <p className= " App-intro">
+            I have changed something here
+            I have changed something here
+            I have changed something here
+            I have changed something he
+            v
+        </ p>
+      </div>
     );
   }
 }
